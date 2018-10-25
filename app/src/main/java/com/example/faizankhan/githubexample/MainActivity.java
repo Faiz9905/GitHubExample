e30b7438da8ad6f4901fd1484119b02e2ddfc001
Faizan Khan <kfs_faizan@yahoo.com>
2018-10-25 15:44:22 +0530

    First Test Second Test

--- app/src/main/java/com/example/faizankhan/githubexample/MainActivity.java
+++ app/src/main/java/com/example/faizankhan/githubexample/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.faizankhan.githubexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.textView);
+
 
     }
 }
